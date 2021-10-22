1de92f9cfd3286723a683500a7c4e1afc9fe53eb
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-10-22 10:17:08 -0700

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import Home from './components/pages/Home'
+import Show from './components/pages/Show'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
 
-export default App;
+const App = () => (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/show" component={Show} exact />
+      </Switch>
+    </ApolloProvider>
+  </BrowserRouter>
+)
+export default App
---
+++ client/src/components/pages/Show.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Show = () => {
+  return (
+    <div className="container">
+      <h1>Show</h1>
+    </div>
+  )
+}
+
+export default Show
